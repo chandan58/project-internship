31ac5b709d3aa8b9245ca3db2e3f1f118c8818d3
chandan58 <daschandan547@gmail.com>
2022-03-03 01:51:51 +0530

    assignment/populate-reference

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://chandandas:CsL3xmXiGHNnwran@cluster0.3q2ao.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
